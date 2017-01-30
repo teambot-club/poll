e8df6ac88d856c6ea796ac4718e88d7e276508ba
Teodor Bozhikov <t_bozhikov@yahoo.com>
2017-01-30 19:00:25 +0200

    initial reply after correct poll command

--- poll.js
+++ poll.js
@@ -2,7 +2,24 @@
 
 var Poll = function(controller) {
 
-    var poll = function(bot, message) {
+    var consts = {
+            indexBullets: [
+                ":zero:",
+                ":one:",
+                ":two:",
+                ":three:",
+                ":four:",
+                ":five:",
+                ":six:",
+                ":seven:",
+                ":eight:",
+                ":nine:"
+            ],
+            space: " ",
+            newLine: "\n"
+        },
+
+        poll = function(bot, message) {
             bot.api.reactions.add({
                 timestamp: message.ts,
                 channel: message.channel,
@@ -13,16 +30,49 @@ var Poll = function(controller) {
                 }
             });
 
-            var args = message.text.match(/".*?"/g),
-                q = args[0],
-            ;
+            try {
+                var args = getPollArgs(message);
+                var q = args[0];
 
-            // handle missing args? 
-            // if only question is present, ask for choices
-            // if only one choice is present, ask for more?
+                // handle missing args? 
+                // if only question is present, ask for choices
+                // if only one choice is present, ask for more?
 
-            bot.reply(message, q + '/n test');
+                var reply = q + consts.newLine;
+                for (var i = 1; i < args.length; i++) {
+                    reply += consts.indexBullets[i] + consts.space;
+                    reply += args[i] + consts.newLine;
+                }
 
+                bot.reply(message, reply);
+                //bot.reply(message, q + '\n test');
+                //bot.reply(message, '\n line 2');
+            } catch (ex) {
+                bot.reply(message, "Oops, exception " + ex);
+            }
+        },
+
+        // returns all quoted strings from message as array of args 
+        getPollArgs = function(message) {
+            var args = [],
+                result = [],
+                regex = /"((?:\\.|[^"\\])*)"/g;
+
+            args = message.text.match(regex);
+
+            for (var i = 0; i < args.length; i++) {
+                var current = args[i].trim();
+
+                if (current.charAt(0) === '"' && current.charAt(current.length - 1) === '"') {
+                    current = current.substr(1, current.length - 2);
+                }
+
+                if (current) {
+                    result.push(current);
+                }
+            }
+
+            return result;
         },
 
         callMe = function(bot, message) {
